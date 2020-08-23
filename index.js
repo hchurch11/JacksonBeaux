9530330332e03706fd24cc601ec472e6a5e160b7
Heather Church <heather@MacBook-Pro-3.local>
2020-08-23 14:49:16 -0600

    Add css, html, and js file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var txt = "";
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.status == 200 && xmlhttp.readyState == 4) {
+    txt = xmlhttp.responseText;
+  }
+};
+xmlhttp.open("GET", "abc.txt", true);
+xmlhttp.send();
 //click
 $("button").click(function () {
   $(this).parents(".grid_item").toggleClass("active");
